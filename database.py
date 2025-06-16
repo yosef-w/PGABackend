4638971c692a834a7852a0ad5dd1dbc2f4380aeb
yosef-w <ywolday24@gmail.com>
2025-06-16 16:27:44 -0500

    add environment varialble

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://parkinguser:zeno24@localhost:5433/parkingdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
