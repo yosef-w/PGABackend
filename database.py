df8275ebf2a6607a2be726bd5bae30a3cf891905
yosef-w <ywolday24@gmail.com>
2025-06-16 16:36:28 -0500

    Fix .env syntax for DATABASE_URL

--- database.py
+++ database.py
@@ -6,7 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Get database URL from environment variable
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set!")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
